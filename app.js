286c13601e37cb9526f2300f7e30215709126c7c
MINWOOPARK <minwoopark@MacBookPro.local>
2020-12-23 14:57:52 +0900

    test dev to branch

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("반갑습니다 해돋이님 리모트 주소가 바껴도 잘 작동할까요?");
+  res.send(
+    "반갑습니다 해돋이님 리모트 주소가 바껴도 잘 작동할까요? dev -> feature4"
+  );
 });
 
 app.listen(5000, () => {
