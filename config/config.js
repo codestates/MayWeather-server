9a941abb01939ca35a4c2e7518dd757f757d6923
Minwoo Park <positive1attraction@gmail.com>
2021-04-12 18:10:25 +0900

    [Fix] development version environment variables

--- config/config.js
+++ config/config.js
@@ -3,12 +3,12 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME_ENV,
+    password: process.env.DATABASE_PASSWORD_ENV,
+    database: process.env.DATABASE_NAME_ENV,
+    host: process.env.DATABASE_HOST_ENV,
     dialect: "mysql",
-    port: process.env.DATABASE_PORT,
+    port: process.env.DATABASE_PORT_ENV,
   },
   test: {
     username: process.env.DATABASE_USERNAME,
